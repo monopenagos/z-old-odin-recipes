ad057ed286e0a9d1a35f8c77c60c6b1653570fa7
Juan Penagos <juancpenagos@gmail.com>
2022-02-14 13:17:59 -0500

    Llevando Nightdrive.js

---
+++ NightDrive.js
@@ -0,0 +1,16 @@
+// summer lasts forever
+async function synthWave() {
+    if (
+        new Date().getFullYear() == 1984 &&
+        currentLocation == 'rooftop' &&
+        weatherConditions == TORRENTIAL_RAIN
+            ) {
+
+                await theNight();
+                const SaxSolo = new Saxophone();
+                SaxSolo.play();
+
+            }
+
+
+}
